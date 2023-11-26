377a5ced08c98bbfb9494037c8b17396ac6a6ece
Iconart <okequadri43@gmail.com>
2023-11-26 10:51:09 +0100

    generate the address and private key for wallet

--- server/index.js
+++ server/index.js
@@ -7,9 +7,9 @@ app.use(cors());
 app.use(express.json());
 
 const balances = {
-  "0x1": 100,
-  "0x2": 50,
-  "0x3": 75,
+  "6a7116c0d6d1543d3c8b3b10184bee01efef58e3": 100, //privatekey:ad69224fd2fd591017a4d2a3aaee7046c7dee5574574190140f5ddffd3ec8140
+  "67f448c50a3a3f2a660ba8f501b64be2e624dbfd": 50, //privateKey:a5ce669cda0372f1eb9ff67e0bb22c28a1f5ffdf5ffd8d74727a3007eb0b49ee
+  "e3e69c71c1fc45e758418d3c288cd548df4bee22": 75, //privateKey:b57eae8b8b1523fbf535e6010ac75a4d5c68d5a3f61abfc3ac4ef4d6d7382178
 };
 
 app.get("/balance/:address", (req, res) => {
---
+++ server/scripts/generate.js
@@ -0,0 +1,10 @@
+const secp = require("ethereum-cryptography/secp256k1");
+const { toHex } = require("ethereum-cryptography/utils");
+const { keccak256 } = require("ethereum-cryptography/keccak")
+
+const privateKey = secp.secp256k1.utils.randomPrivateKey();
+console.log("private Key: ", toHex(privateKey));
+
+const publicKey = secp.secp256k1.getPublicKey(privateKey);
+const address = toHex(keccak256(publicKey.slice(1)).slice(-20));
+console.log("public Key: ", address);
