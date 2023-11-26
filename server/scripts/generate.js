a7da54f40af464b7ecf68bf4a36ac79c04cc3a79
Iconart <okequadri43@gmail.com>
2023-11-26 11:13:45 +0100

    generate the privateKey and address

--- server/scripts/generate.js
+++ server/scripts/generate.js
@@ -1,7 +1,9 @@
 const secp = require("ethereum-cryptography/secp256k1");
 const { toHex } = require("ethereum-cryptography/utils");
 const { keccak256 } = require("ethereum-cryptography/keccak")
-
+//get the private Key
+//get the public key for the private key
+//convert the public to an ethereum wallet address
 const privateKey = secp.secp256k1.utils.randomPrivateKey();
 console.log("private Key: ", toHex(privateKey));
 
