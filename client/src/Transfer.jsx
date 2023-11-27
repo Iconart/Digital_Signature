54e2de3004ed816ee100f46cf4f783b02b287673
Iconart <okequadri43@gmail.com>
2023-11-27 14:36:19 +0100

    import all required packages  to verify the sender address

--- client/src/Transfer.jsx
+++ client/src/Transfer.jsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
 import server from "./server";
+import * as secp  from "ethereum-cryptography/secp256k1";
+import { toHex, uft8ToBytes} from "ethereum-cryptography/utils";
+import { keccak256 } from "ethereum-cryptography/keccak";
 
 
 function Transfer({ address, setBalance, privateKey }) {
--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = 3042;
+const secp = require("ethereum-cryptography/secp256k1");
+
 
 
 
